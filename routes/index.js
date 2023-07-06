bea0104317fa106795e3762ba59c3813fb9d454b
Yu Cheng Li <ychengli11065@gmail.com>
2023-07-06 15:32:56 -0700

    finished tutorial

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Message Board", messages: messages });
+});
+
+router.post("/new", (req, res, next) => {
+  const newMessage = {
+    text: req.body.text,
+    user: req.body.username,
+    added: new Date(),
+  };
+
+  messages.push(newMessage);
+  res.redirect("/");
 });
 
 module.exports = router;
